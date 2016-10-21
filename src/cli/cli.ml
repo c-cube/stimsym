ba985b5796ff9747aa1ec44c7689576267e4af56
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-10-21 20:20:28 +0200

    add `-v` option to CLI

--- src/cli/cli.ml
+++ src/cli/cli.ml
@@ -5,7 +5,11 @@
 
 open Rewrite
 
-let rec main_loop () =
+type config = {
+  verbose: bool;
+}
+
+let rec main_loop ~config () =
   match LNoise.linenoise "> " with
     | None -> ()
     | Some res ->
@@ -14,20 +18,35 @@ let rec main_loop () =
       begin match Parser.parse_expr Lexer.token buf with
         | e ->
           ignore (LNoise.history_add res);
-          Format.printf "parsed: @[%a@]@." Expr.pp_full_form e;
+          if config.verbose then (
+            Format.printf "parsed: @[%a@]@." Expr.pp_full_form e;
+          );
+          let e' = Eval.eval e in
+          Format.printf "@[%a@]@." Expr.pp_full_form e'; (* TODO: use nice printer instead *)
         | exception e ->
           Format.printf "error: %s@." (Printexc.to_string e);
       end;
-      main_loop()
+      main_loop ~config ()
 
 let history_file =
   try Some (Filename.concat (Sys.getenv "HOME") ".rewrite-history")
   with _ -> None
 
 let () =
+  let verbose = ref false in
+  Arg.parse
+    [ "-v", Arg.Set verbose, " enable verbose output";
+    ] (fun _ -> ())
+    "./cli [options]
+
+    A simple command-line REPL for rewriting expressions.
+    ";
+  let config = {
+    verbose= !verbose;
+  } in
   ignore (LNoise.history_set ~max_length:1000);
   CCOpt.iter (fun f -> ignore (LNoise.history_load ~filename:f)) history_file;
-  main_loop();
+  main_loop ~config ();
   CCOpt.iter (fun f -> ignore (LNoise.history_save ~filename:f)) history_file;
   ()
 
