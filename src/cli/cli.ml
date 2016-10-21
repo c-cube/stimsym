dd7b29750ed5921166843e28f8f10432a9027e5a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-10-21 19:46:29 +0200

    improve cli readline

--- src/cli/cli.ml
+++ src/cli/cli.ml
@@ -12,11 +12,21 @@ let rec main_loop () =
       let buf = Lexing.from_string res in
       begin match Parser.parse_expr Lexer.token buf with
         | e ->
+          ignore (LNoise.history_add res);
           Format.printf "parsed: @[%a@]@." Expr.pp_full_form e;
         | exception e ->
           Format.printf "error: %s@." (Printexc.to_string e);
       end;
       main_loop()
 
+let history_file =
+  try Some (Filename.concat (Sys.getenv "HOME") ".rewrite-history")
+  with _ -> None
+
 let () =
-  main_loop()
+  ignore (LNoise.history_set ~max_length:1000);
+  CCOpt.iter (fun f -> ignore (LNoise.history_load ~filename:f)) history_file;
+  main_loop();
+  CCOpt.iter (fun f -> ignore (LNoise.history_save ~filename:f)) history_file;
+  ()
+
