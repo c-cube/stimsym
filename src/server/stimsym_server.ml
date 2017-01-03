0fac1cff7cdeeff7f0967240a18f5d4447f914cd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-01-03 14:41:36 +0100

    use `Jupyter_kernel.Client_main`

--- src/server/stimsym_server.ml
+++ src/server/stimsym_server.ml
@@ -4,51 +4,11 @@
 (** {1 Notebook interface} *)
 
 open Stimsym
-open Lwt.Infix
 
 module H = Tyxml.Html
 module C = Jupyter_kernel.Client
+module Main = Jupyter_kernel.Client_main
 module Log = Jupyter_kernel.Log
-module Proto_j = Jupyter_kernel.Protocol_j
-
-let output_cell_max_height = ref "100px"
-
-(*******************************************************************************)
-(* command line *)
-
-let connection_file_name = ref ""
-let completion = ref false
-let object_info = ref false
-let init_file = ref ""
-
-let ci_stdin = ref 50000
-let ci_shell = ref 50001
-let ci_iopub = ref 50002
-let ci_heartbeat = ref 50003
-let ci_control = ref 50004
-let ci_transport = ref "tcp"
-let ci_ip_addr = ref ""
-
-let () =
-  Arg.(parse
-      (align [
-          "--log", String (Log.open_log_file), " <file> open log file";
-          "--connection-file", Set_string(connection_file_name),
-          " <filename> connection file name";
-          "--init", Set_string(init_file), " <file> load <file> instead of default init file";
-          "--completion", Set(completion), " enable tab completion";
-          "--object-info", Set(object_info), " enable introspection";
-          (* pass connection info through command line *)
-          "--ci-stdin", Set_int(ci_stdin), " (connection info) stdin zmq port";
-          "--ci-iopub", Set_int(ci_iopub), " (connection info) iopub zmq port";
-          "--ci-shell", Set_int(ci_shell), " (connection info) shell zmq port";
-          "--ci-control", Set_int(ci_control), " (connection info) control zmq port";
-          "--ci-heartbeat", Set_int(ci_heartbeat), " (connection info) heartbeat zmq port";
-          "--ci-transport", Set_string(ci_transport), " (connection info) transport";
-          "--ci-ip", Set_string(ci_ip_addr), " (connection info) ip address"
-        ])
-      (fun s -> failwith ("invalid anonymous argument: " ^ s))
-      "stimsym kernel")
 
 (** {2 Execution of queries} *)
 
@@ -156,59 +116,8 @@ let kernel : C.Kernel.t =
     ~complete: (fun ~pos msg -> Lwt.return (complete pos msg))
     ()
 
-(*******************************************************************************)
 (* main *)
 
-let () = Printf.printf "[stimsym] Starting kernel\n%!"
-(*let () = Sys.interactive := false*)
-let () = Unix.putenv "TERM" "" (* make sure the compiler sees a dumb terminal *)
-
-let connection_info =
-  if !ci_ip_addr <> "" then
-    (* get configuration parameters from command line *)
-    { Proto_j.
-      stdin_port = !ci_stdin;
-      ip = !ci_ip_addr;
-      control_port = !ci_control;
-      hb_port = !ci_heartbeat;
-      signature_scheme = "hmac-sha256";
-      key = "";
-      shell_port = !ci_shell;
-      transport = !ci_transport;
-      iopub_port = !ci_iopub;
-    }
-  else
-    (* read from configuration files *)
-    let f_conn_info =
-      try open_in !connection_file_name
-      with _ ->
-        failwith ("Failed to open connection file: '" ^
-            !connection_file_name ^ "'")
-    in
-    let state = Yojson.init_lexer () in
-    let lex = Lexing.from_channel f_conn_info in
-    let conn = Proto_j.read_connection_info state lex in
-    let () = close_in f_conn_info in
-    conn
-
-let rec main_loop () =
-  try%lwt
-    let sockets = Jupyter_kernel.Sockets.open_sockets connection_info in
-    let key = connection_info.Proto_j.key in
-    let key = if key="" then None else Some key in
-    let sh = C.make ?key sockets kernel in
-    C.run sh;
-    >>= function
-    | C.Run_stop ->
-      Log.log "Done.\n";
-      Lwt.return_unit
-    | C.Run_restart ->
-      main_loop ()
-  with e ->
-    Log.log (Printf.sprintf "Exception: %s\n" (Printexc.to_string e));
-    Log.log "Dying.\n";
-    exit 0
-
 let () =
-  Log.log "start main...\n";
-  Lwt_main.run (main_loop ())
+  Lwt_main.run
+    (Main.main ~usage:"stimsym" kernel)
